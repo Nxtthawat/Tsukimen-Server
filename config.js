b2dc5f7824900e9748e7591a1262cd7c6a0cb6cc
pupparn <84955230+Nxtthawat@users.noreply.github.com>
2024-08-09 13:03:59 +0700

    changed config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    secureKey: 'tsukimen@lampang',
+    tsukimenDB: {
+        host: 'bymallljw7sqdngpgui9-mysql.services.clever-cloud.com',
+        user: 'uk91duwmgmcd3xpa',
+        password: 'HOxFRCBqfh5yHrMTUfdB',
+        database: 'bymallljw7sqdngpgui9'
+    }
+};
+
+export default config;
